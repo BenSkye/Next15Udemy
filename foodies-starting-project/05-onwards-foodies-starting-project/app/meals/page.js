49a8648b19361d77a5a83654fde67b410224e04c
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-12-02 19:28:35 +0700

    lession 1

---
+++ foodies-starting-project/05-onwards-foodies-starting-project/app/meals/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Meals() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>Meals</h1>
+      <Link href={'/meals/share'}>
+        <button> Share</button>
+      </Link>
+    </main>
+  );
+}
